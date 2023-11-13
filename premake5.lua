6c080bc267411a2e4dd980fe6223507f926c408c
Yannik Hodel <yannik.hodel@siemens.com>
2023-11-13 16:11:30 +0100

    Fix Premake File

--- premake5.lua
+++ premake5.lua
@@ -1,35 +1,38 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+    staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
 		"imgui.cpp",
+		"misc/cpp/imgui_stdlib.cpp",
 		"imgui_draw.cpp",
-		"imgui_internal.h",
 		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.cpp"
+	}
+
+	includedirs { "./", "%{IncludeDir.Glad}", "%{IncludeDir.GLFW}" }
+
+	defines 
+	{
+		"IMGUI_IMPL_OPENGL_LOADER_GLAD"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
 		cppdialect "C++17"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
@@ -38,3 +41,8 @@ project "ImGui"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
